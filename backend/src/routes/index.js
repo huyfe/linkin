02f23b6650ed39b123af54cffb7c354010d14167
Ngochai20 <ngochai2720@gmail.com>
2021-10-17 13:12:48 +0700

    Set up database table Group

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./Users');
+const groupRouter = require('./Groups');
 
 function route(app) {
     app.use('/users', userRouter);
+    app.use('/groups', groupRouter);
 }
 
 module.exports = route;

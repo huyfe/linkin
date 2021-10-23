8731d0e025ebbcedf59b8fb3a7bb1da80c443f7a
Ngochai20 <ngochai2720@gmail.com>
2021-10-23 23:14:44 +0700

    Create API group and create delete function table group

--- backend/src/app/models/GroupsModel.js
+++ backend/src/app/models/GroupsModel.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const mongoosedelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
+
 
 const Group = new Schema(
     {
+        _id: { type: Number },
         title: { type: String},       
         desc: { type: String },  
         image: { type: String },    
@@ -15,9 +19,18 @@ const Group = new Schema(
         id_author: {type: [{ id: Number }] }
     }, 
     {
-        timestamps: true,
+        _id: false,
+        timestamps: true
     }
 );
 
 
+// // Tự động tăng id khi thêm link vào database
+// Group.plugin(AutoIncrement);
+
+// // mongoose delete plugin
+// Group.plugin(mongoosedelete, {
+//     deletedAt: true,
+//     overrideMethods: 'all'
+// });
 module.exports = mongoose.model('Group', Group);

0d1fd2baad9b764c51300f54429308aef31e46da
Ngochai20 <ngochai2720@gmail.com>
2021-10-24 22:50:45 +0700

    CRUD Api table group

--- backend/src/app/controllers/GroupsController.js
+++ backend/src/app/controllers/GroupsController.js
@@ -1,33 +1,58 @@
-const { mutipleMongooseToObject } = require('../../util/mongoose');
-const { mongooseToObject } = require('../../util/mongoose');
+
 // Nhúng model group
 const Groups = require('../models/GroupsModel');
 
 module.exports = {
-    
-     // Hiển thị tất cả group
-    ShowAllGroups: function(req, res, next){
-        Groups.find({})
-            .then(groups => {
-                res.json({ 
-                    groups: mutipleMongooseToObject(groups)
-                });
-            })
-            .catch(next);
+     
+    // Hiển thị tất cả group -> [GET]/
+    async ShowAllGroups(req, res, next) {
+        await Groups.find({})
+        .then((groups) => res.json(groups))
+        .catch(next);
     },
 
-    //Tạo group
-    CreateGroup: function(req, res, next) {
-        const groups = new Groups(req.body);
-        groups.save()
+    //Thêm group
+    async CreateGroup(req, res, next) {
+        const newGroup = req.body
+        const groups = new Groups(newGroup);
+        await groups.save()
             .then(() => res.send('Tạo group thành công!'))
             .catch(next);
     },
 
-    // Xóa group
-    DeleteGroup: function(req, res, next) {
-        Groups.delete({ _id: req.params.id })
+     // Cập nhật group
+    async UpdateGroup (req, res, next) {
+        await Groups.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.send('Sửa group thành công!'))
+            .catch(next);
+    },
+
+    // Xóa group vĩnh viễn
+    async DeleteGroup (req, res, next) {
+        await Groups.deleteOne({ _id: req.params.id })
             .then(() => res.send('!Xóa group thành công!'))
             .catch(next); // bắt lỗi
+    },
+
+     // Xóa group tạm thời -> [DELETE]/trash-group/:id
+    async TrashGroup (req, res, next) {
+        await Groups.delete({ _id: req.params.id })
+            .then(() => res.send('!Xóa group tạm thời!'))
+            .catch(next); // bắt lỗi
+    },
+
+    //Hiện các group đã xóa tạm thời -> [GET]/show-trash
+    async ShowTrash(req, res, next) {
+        await Groups.findDeleted({})
+        .then((groups) => res.json(groups))
+        .catch(next);
+    },
+
+      // Khôi phục group đã xóa tạm thời
+    async RestoreGroup (req, res, next) {
+        await Groups.restore({ _id: req.params.id })
+            .then(() => res.send('!Khôi phục group đã xóa tạm thời thành công!'))
+            .catch(next); // bắt lỗi
     }
+
 }

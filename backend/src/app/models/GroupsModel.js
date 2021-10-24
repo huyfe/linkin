26150f1a2ab4c3a7c6034ba96a4a4324f3d5764c
Ngochai20 <ngochai2720@gmail.com>
2021-10-24 18:37:38 +0700

    Update table group

--- backend/src/app/models/GroupsModel.js
+++ backend/src/app/models/GroupsModel.js
@@ -3,14 +3,13 @@ const mongoosedelete = require('mongoose-delete');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
-
 const Group = new Schema(
     {
         _id: { type: Number },
         title: { type: String},       
         desc: { type: String },  
         image: { type: String },    
-        created_date: { type: String },
+        created_date: { type: Date },
         public: { type: String },
         id_links: {type: [{ id: Number }] },
         id_members: {type: [{ id: Number }] },
@@ -26,11 +25,11 @@ const Group = new Schema(
 
 
 // // Tự động tăng id khi thêm link vào database
-// Group.plugin(AutoIncrement);
+Group.plugin(AutoIncrement,  {collection_name: 'counter_groups' });//tạo thêm collection
 
-// // mongoose delete plugin
-// Group.plugin(mongoosedelete, {
-//     deletedAt: true,
-//     overrideMethods: 'all'
-// });
+// mongoose delete plugin
+Group.plugin(mongoosedelete, {
+    deletedAt: true,
+    overrideMethods: 'all'
+});
 module.exports = mongoose.model('Group', Group);
--- backend/src/routes/Groups.js
+++ backend/src/routes/Groups.js
@@ -5,6 +5,8 @@ const groupController = require('../app/controllers/GroupsController');
 
 // Show all api table group
 router.get('/', groupController.ShowAllGroups);
+// Tạo group
+router.post('/create-group', groupController.CreateGroup);
 
 // Xóa group
 router.delete('/delete-group/:id', groupController.DeleteGroup);

fc9b0c2a57244c64e1bbed3c55161da8c891d775
Leader <huyyntran@gmail.com>
2021-10-29 23:20:52 +0700

    Create socker client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/index';
 import Footer from './components/Footer/index';
@@ -6,8 +6,26 @@ import Header from './components/Header/index';
 import Register from './components/User/Register/index';
 import Login from './components/User/Login';
 import AnotherLogin from './components/User/Login/AnotherLogin';
+import { io } from "socket.io-client";
 
 function App() {
+    useEffect(() => {
+        const socket = io('ws://localhost:3000');
+
+        socket.on('connnection', () => {
+            console.log('connected to server');
+        })
+
+        socket.on('message', (message) => {
+            console.log(message);
+        })
+
+        socket.on('disconnect', () => {
+            console.log('Socket disconnecting');
+        })
+
+    }, []);
+
     return (
         <div className="App">
             <Router>

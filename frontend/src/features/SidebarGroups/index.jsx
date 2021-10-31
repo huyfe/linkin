a05a7a57ac84bac0f5764251c21932aae7c9c4ea
NGOCHAI27 <ngochai2720@gmail.com>
2021-11-01 00:09:41 +0700

    Update style groups

--- frontend/src/features/SidebarGroups/index.jsx
+++ frontend/src/features/SidebarGroups/index.jsx
@@ -7,33 +7,29 @@ export default function SidebarGroupsJoin() {
 
         <section id="sidebarGroups" className="col-lg-3" >
             <div className="box__group">
-                <div class="input-group rounded">
-                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                    aria-describedby="search-addon" />
-                    <span class="input-group-text border-0" id="search-addon">
-                        <i class="fas fa-search"></i>
-                    </span>
+                <div class="input-search">
+                    <input type="search" name="" id="input-search" placeholder="Tìm kiếm nhóm" class="reset-btn" />
+                    <label class="icon__search" for="input-search"><i class="fal fa-search"></i></label>
                 </div>
-                <h4 className="title">Nhóm bạn quản lí</h4>
-                <ul className="list__item_group">
-                    <li className="item__group">
-                        <img src="images/Groups/image_group_one.png" alt="image_group_one" />
-                        <a href="#">Cộng đồng Nodejs</a>
-                    </li>
-                    <li className="item__group">
-                    <img src="images/Groups/image_group_two.png" alt="image_group_one" />
-                        <a href="#">Cộng đồng Htm/Css/Javascrpit</a>
-                      
-                    </li>
-                    <li className="item__group">
-                        <img src="images/Groups/image_group_three.png" alt="image_group_one" />
-                        <a href="#">Cộng đồng Front-end</a>
-                    </li>
-                </ul>
-                <button className="item__button">
-                    <a href="#">Tạo nhóm mới</a>
-                    <img src="images/Groups/icon_plus.svg" alt="icon_plus" />
-                </button>
+                <div className="group__manager">
+                    <h4 className="title">Nhóm bạn quản lí</h4>
+                    <ul className="list__item_group">
+                        <li className="item__group">
+                            <img src="images/Groups/image_group_one.png" alt="image_group_one" />
+                            <a href="#">Cộng đồng Nodejs</a>
+                        </li>
+                        <li className="item__group">
+                            <img src="images/Groups/image_group_two.png" alt="image_group_one" />
+                            <a href="#">Cộng đồng Htm/Css/Javascrpit</a>
+
+                        </li>
+                        <li className="item__group">
+                            <img src="images/Groups/image_group_three.png" alt="image_group_one" />
+                            <a href="#">Cộng đồng Front-end</a>
+                        </li>
+                    </ul>
+                    <button className="item__button">Tạo nhóm mới<i class="far fa-plus"></i></button>
+                </div>
             </div>
         </section>
 

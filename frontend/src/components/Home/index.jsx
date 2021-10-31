b5eac224ba93605619fafedf35f218ea67a9eba4
Travis Do <badat336@gmail.com>
2021-10-31 23:00:03 +0700

    Update font-awesome and bootstrap

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import UserSideBar from './user-sidebar';
+import GroupSideBar from './group-sidebar';
 import './_style.scss';
 
 export default function Home() {
     return (
-        <section id="homepage d-flex justify-content-center">
-            <h2>Đây là trang chủ</h2>
-        </section>
+        <div className="container-fluid">
+            <div className="row">
+                <GroupSideBar />
+                <section id="homepage" className="d-flex justify-content-center col-lg-6">
+                    <h2>Đây là trang chủ</h2>
+                </section>
+                <UserSideBar />
+            </div>
+        </div>
     );
 }

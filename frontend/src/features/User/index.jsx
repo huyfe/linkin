fa66ed7f05e6463784f44d25f0f69ec7eb8d1038
Leader <huyyntran@gmail.com>
2021-11-01 21:16:58 +0700

    Add font roboto

---
+++ frontend/src/features/User/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+index.propTypes = {
+
+};
+
+function index(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default index;

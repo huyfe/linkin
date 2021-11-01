565e7b61c1971dfe76b00c9f027ceaae49d7bff5
NGOCHAI27 <ngochai2720@gmail.com>
2021-11-01 22:59:58 +0700

    Update folder group

--- frontend/src/features/Group/components/HomeGroupComponent/index.jsx
+++ frontend/src/features/Group/components/HomeGroupComponent/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import SidebarGroups from '../SidebarGroupComponent';
 import SidebarGroupsJoin from '../SidebarGroupJoinComponent';
+import TimelineGroup from '../TimelineComponent';
 
 
 function MainGroup(props) {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <SidebarGroups />
-                <SidebarGroupsJoin />
+
+        <div className="group">
+            <div className="wrapper">
+                <div className="row">
+                    <div className="col-lg-3">
+                        <SidebarGroups />
+                    </div>
+                    <div className="col-lg-9">
+                        <div className="group__theme">
+                        <img src="images/Groups/image_theme.png" />
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-8">
+                                <TimelineGroup />
+                            </div>
+                            <div className="col-lg-4">
+                                <SidebarGroupsJoin />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
---
+++ frontend/src/features/Group/components/TimelineComponent/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss';
+
+
+ function TimelineGroup() {
+    return (
+
+        <section id="timelineGroup">
+            <div className="timeline__group">
+                <h1>đây là time line</h1>
+            </div>
+        </section>
+
+    )
+}
+
+export default TimelineGroup;

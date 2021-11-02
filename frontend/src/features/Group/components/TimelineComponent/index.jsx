5fca4639f2eca864b22ce7af639657c6594f1ce4
Travis Do <badat336@gmail.com>
2021-11-02 21:40:42 +0700

    Group Component

--- frontend/src/features/Group/components/TimelineComponent/index.jsx
+++ frontend/src/features/Group/components/TimelineComponent/index.jsx
@@ -17,11 +17,35 @@ function TimelineGroup() {
                 </div>
 
                 <div className="box-post">
-                    <div className="info-post">
+                    <div className="box-post-info d-flex align-items-center">
                         <div className="avatar">
                             <img src="images/Timeline/huythanhxuan.jpg" />
                         </div>
-                        .ì
+                        <div className="infor-user">
+                            <h4>Quốc Huy</h4>
+                            <div className="info__post d-flex">
+                                <div className="info__post-date d-flex align-items-center">
+                                   <p> 25 tháng 9 </p> <span class="icon-earth"></span>
+                                </div>
+                                <div className="info__post-group d-flex align-items-center">
+                                    <div className="box-img">
+                                        <img src="images/Timeline/r1.png" />
+                                    </div>
+                                    <p className="m-0">Nguyên lý cơ bản design</p>
+                                </div>  
+                            </div>
+                        </div>
+                        <button className="option">
+                            <span class="icon-more-horizontal"></span>
+                        </button>
+                    </div>
+                    <div className="content-one m-3">
+                        <p>Các nguyên lý cơ bản của design ...</p>
+                    </div>
+                    <div className="content-two">
+                        <div className="link">
+                            <p>www.linkin.com/abcxyx</p>
+                        </div>
                     </div>
                 </div>
             </div>

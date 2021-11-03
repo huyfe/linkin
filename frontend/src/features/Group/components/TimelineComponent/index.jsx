9220638dcfe9cafd06e0ff975b19f5b19f6a14e3
Travis Do <badat336@gmail.com>
2021-11-04 02:00:22 +0700

    Timeline Component

--- frontend/src/features/Group/components/TimelineComponent/index.jsx
+++ frontend/src/features/Group/components/TimelineComponent/index.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 import './style.scss';
 import { Link } from 'react-router-dom';
+import LinkComponent from '../LinkComponent';
 
 function TimelineGroup() {
+    const Posts = [
+        {   
+            id: 1,
+            imageUser: 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2787&q=80',
+            userLink: "/do-ba-dat",
+            nameUser: "Bá Đạt",
+            datePost: "20/10/2019",
+            
+            groupLink: "/cong-dong-frontend-vietnam",
+            imageGroup: "/images/Timeline/group__thumb-1.png",
+            nameGroup: "Cộng đồng Frontend Việt Nam",
+            contentLink: "www.linkin.com/abcxyx",
+            contentDesc: "Các nguyên lý cơ bản của lập trình...",
+            imageLink: "/images/Timeline/post-thumb.png",
+            like: 20,
+            comment: 30,
+            hour: 40,
+        },
+        {
+            id: 2,
+            imageUser: 'https://images.unsplash.com/photo-1604004555489-723a93d6ce74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80',
+            userLink: "/do-ba-dat",
+            nameUser: "Bá Đạt 2",
+            datePost: "20/10/2019",
+
+            groupLink: "/cong-dong-frontend-vietnam",
+            imageGroup: "/images/Timeline/group__thumb-1.png",
+            nameGroup: "Cộng đồng Frontend Việt Nam",
+            contentLink: "www.linkin.com/abcxyx",
+            contentDesc: "Các nguyên lý cơ bản của lập trình...",
+            imageLink: "/images/Timeline/post-thumb.png",
+            like: 20,
+            comment: 30,
+            hour: 40,
+        },
+
+    ];
+    const listPost = Posts.map(post => {
+        return (
+            <LinkComponent
+                imageUser={post.imageUser}
+                userRef={post.userProfileRef}
+                nameUser={post.nameUser}
+                datePost={post.datePost}
+                groupRef={post.groupRef}
+                imageGroup={post.imageGroup}
+                nameGroup={post.nameGroup}
+                contentDesc={post.contentDesc}
+                contentLink={post.contentLink}
+                imageLink={post.imageLink}
+                like={post.like}
+                comment={post.comment}
+                hour={post.hour}
+            ></LinkComponent>
+        );
+    });
+
     return (
         <section id="timelineGroup">
             <div className="timeline__group">
@@ -141,6 +199,7 @@ function TimelineGroup() {
                         </div>
                     </div>
                 </div>
+                {listPost}
             </div>
         </section>
     )

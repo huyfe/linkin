e611ff422ff5085ae84a0543cb82b27c127e4279
LeQuocLap5201 <quoclap5201@gmail.com>
2021-11-06 01:26:42 +0700

    giao diện 2 tab Người theo dõi và Đang theo dõi

---
+++ frontend/src/features/User/components/ProfileComponent/SearchFollower/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.scss";
+
+SearchFollower.propTypes = {};
+
+function SearchFollower(props) {
+  return (
+    <div className="d-flex align-items-center searchFollower">
+      <h3>50 Người</h3>
+      <div className="searchFollower__group">
+        <label
+          htmlFor="searchFollower"
+          className="d-flex justify-content-end align-items-center"
+        >
+          <i class="fal fa-search"></i>
+        </label>
+        <input id="searchFollower" type="search" />
+      </div>
+    </div>
+  );
+}
+
+export default SearchFollower;

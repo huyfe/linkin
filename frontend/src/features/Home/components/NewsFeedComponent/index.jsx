d897f335a10faf2ded452b193595964d1a7622a2
Leader <huyyntran@gmail.com>
2021-11-08 23:50:07 +0700

    Move UploadLinkComponent to common component

--- frontend/src/features/Home/components/NewsFeedComponent/index.jsx
+++ frontend/src/features/Home/components/NewsFeedComponent/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import UpLoadLinkComponent from "../UploadLinkComponent";
+import UpLoadLinkComponent from "../../../../components/UploadLinkComponent";
 
 NewsFeedComponent.propTypes = {
 

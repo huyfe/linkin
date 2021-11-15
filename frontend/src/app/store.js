af355c14c549366d57b911db7d6e250e77eceaca
Henry Vo <henry.hai@namtech.sg>
2021-11-15 21:44:31 +0700

    Show api group

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,11 @@
 import linkReducer from "../features/Link/linkSlice";
+import groupReducer from "../features/Group/groupSlice";
+
 const { configureStore } = require("@reduxjs/toolkit");
 
 const rootReducer = {
     link: linkReducer,
+    group: groupReducer
 };
 
 const store = configureStore({

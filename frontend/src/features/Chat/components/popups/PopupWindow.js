a545317d58b9fa6f9fadb09fc15e289cb3a57940
unknown <headblbpzz3@gmail.com>
2021-11-16 18:44:09 +0700

    .

---
+++ frontend/src/features/Chat/components/popups/PopupWindow.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class PopupWindow extends Component {
+    componentDidMount() {
+        this.scLaucher = document.querySelector('#sc-launcher');
+        this.scLauncher = addEventListener('click', this.interceptLaucherClick);
+    }
+    componentWillUnmount() {
+        this.scLauncher.removeEventListener('click', this.interceptLaucherClick);
+    }
+    interceptLaucherClick = (e) {
+        const { isOpen } = this.props;
+        const clickedOutside = !this.emojiPopup.contains(e.target) && isOpen;
+        clickedOutside && this.propsonClickOutside(e);
+    }
+
+    render() {
+        const {isOpen, children } = this.props;
+        return (
+            <div className="sc-popup-window" ref={e => this.emojiPopup = e}>
+                <div className={`sc-popup-window--cointainer ${isOpen ? '' : 'closed'}`}>
+                    <input
+                        onChange={this.props.onInputChange}
+                        className="sc-popup-window--search"
+                        placeholder="Search emoji..."
+                    />
+                    {children}
+                    </div>
+                </div>
+            );
+        }
+}
+
+export default PopupWindow;
